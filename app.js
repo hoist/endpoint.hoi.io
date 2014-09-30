273b073e2a52c1c8350a618fed7b0aba38b0a327
Owen Evans <owen@bgeek.net>
2014-09-30 21:47:40 +1300

    allow graceful reload

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 'use strict';
+
 var server = require('./lib/server');
+
+process.on('message', function (msg) {
+  if (msg === 'shutdown') {
+    // Your process is going to be reloaded
+    // You have to close all database/socket.io/* connections
+
+    console.log('Closing all connections...');
+
+    // You will have 4000ms to close all connections before
+    // the reload mechanism will try to do its job
+    server.stop(function () {
+      process.exit(0);
+    });
+  }
+});
+
+
 server.start();
 console.log('started');
